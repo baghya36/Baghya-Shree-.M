b60cd2dd03cddc0a45c739f46e59c80d67004182
baghya36 <112693856+baghya36@users.noreply.github.com>
2022-10-05 12:50:40 +0530

    Add files via upload

---
+++ DoWhileLoop.java
@@ -0,0 +1,20 @@
+package DoWhileLoop;
+
+public class DoWhileLoop {
+
+	public static void main(String[] args) {
+		int x1;
+		
+		do {
+			System.out.print("value of X : " + X );
+			X+;
+			System.out.print("\n");
+		}While(X < 11 );
+		
+		
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
